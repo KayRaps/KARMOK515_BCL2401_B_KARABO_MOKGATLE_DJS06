4405bfca61b06078aa746d68f4b0aab00842cd71
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-05-24 15:29:38 +0200

    cleaned the code

--- scripts.js
+++ scripts.js
@@ -1,13 +1,20 @@
 // A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
+const provinces = [
+  "Western Cape",
+  "Gauteng",
+  "Northern Cape",
+  "Eastern Cape",
+  "KwaZulu-Natal",
+  "Free State",
+];
 
 // Log each province to the console
-provinces.forEach(province => {
+provinces.forEach((province) => {
   console.log(province);
-})
+});
 
 // Array with province names in all uppercase
-const provinceUppercase = provinces.map(province => province.toUpperCase());
+const provinceUppercase = provinces.map((province) => province.toUpperCase());
 console.log(provinceUppercase);
 
 // Sort the provinces alphabetically
@@ -15,18 +22,26 @@ const sortedProvinces = provinces.sort();
 console.log(sortedProvinces);
 
 // Filter for filtering out provinces conataining "Cape"
-const provincesWithoutCape = provinces.filter(province => !province.includes('Cape'));
+const provincesWithoutCape = provinces.filter(
+  (province) => !province.includes("Cape")
+);
 const reaminingProvincesCount = provincesWithoutCape.length;
 console.log(reaminingProvincesCount);
 
-
 // A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
+const names = [
+  "Ashwin",
+  "Sibongile",
+  "Jan-Hendrik",
+  "Sifso",
+  "Shailen",
+  "Frikkie",
+];
 
 // Logging each name to the console
-names.forEach(name => {
+names.forEach((name) => {
   console.log(name);
-})
+});
 
 //Logging each name with its matching province
 names.forEach((name, index) => {
@@ -35,11 +50,11 @@ names.forEach((name, index) => {
 });
 
 // array with the length of each name
-const nameLengths = names.map(name => name.length);
+const nameLengths = names.map((name) => name.length);
 console.log(nameLengths);
 
 // Boolean array indicating whether each name contains the letter 'S'
-const containsS = names.map(name => name.toLowerCase().includes('s'));
+const containsS = names.map((name) => name.toLowerCase().includes("s"));
 console.log(containsS);
 
 // Use reduce to create an object mapping names to their respective provinces
@@ -50,38 +65,41 @@ const nameProvinceMapping = names.reduce((acc, name, index) => {
 
 // A list of products with prices:
 const products = [
-  { product: 'banana', price: "2" },
-  { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
-  { product: 'avocado', price: "8" },
-  { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
+  { product: "banana", price: "2" },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: "8" },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
 ];
 
 //1. Logging Products on the console
-console.log(products.forEach(product => console.log(product.product)));
+console.log(products.forEach((product) => console.log(product.product)));
 
 //2. filtering by Name Length
-console.log(products.filter(product => product.product.length <= 5));
-
+console.log(products.filter((product) => product.product.length <= 5));
 
 //3. Price Manipulation
 console.log(
   products
-  .filter(product => product.price && product.price.toString().trim() !== '')
-  .map(product => ({ ...product, price: Number(product.price) }))
-  .reduce((total, product) => total + product.price, 0)
+    .filter(
+      (product) => product.price && product.price.toString().trim() !== ""
+    )
+    .map((product) => ({ ...product, price: Number(product.price) }))
+    .reduce((total, product) => total + product.price, 0)
 );
 
 //4. Concatenate Product Names
-console.log(products.reduce((acc, product) => acc + product.product, ''));
+console.log(products.reduce((acc, product) => acc + product.product, ""));
 
 //5. Find Extemes in Prices
 console.log(
   (() => {
     const pricesArray = products
-    .filter(product => product.price && product.price.toString().trim() !== '')
-    .map(product => Number(product.price));
+      .filter(
+        (product) => product.price && product.price.toString().trim() !== ""
+      )
+      .map((product) => Number(product.price));
 
     const highestPrice = Math.max(...pricesArray);
     const lowestPrice = Math.min(...pricesArray);
@@ -92,7 +110,7 @@ console.log(
 
 // 6. Object Transformation
 console.log(
-  products.map(product => {
+  products.map((product) => {
     const { product: name, price: cost } = product;
     return { name, cost };
   })
